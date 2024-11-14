039855f5974d97f9757c2427bc22055929e2bdb0
Matthew Hoff <matthew.g.hoff@gmail.com>
2024-11-14 14:09:30 -0500

    bug fix re hypenated (sub)folders within .zip download

--- oraic.go
+++ oraic.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -25,7 +26,9 @@ func main() {
 	fmt.Println("-", ORAIC_SDK_NAME)
 
 	OK_DEFAULT_INSTALL := askInstallOK("Accept the default install location?\n - " + ORAIC_DST_PATH + "\nSelect")
-	if !OK_DEFAULT_INSTALL {
+	if OK_DEFAULT_INSTALL {
+		InstallOracleInstantClient(USER_DOWNLOADS, ORAIC_DST_PATH)
+	} else if !OK_DEFAULT_INSTALL {
 		CHANGE_DEFAULT_INSTALL := askChangeDefaultInstall("Change the default install location from '" + ORAIC_DST_PATH + "'? Select")
 		if !CHANGE_DEFAULT_INSTALL {
 			CONT_DEFAULT_INSTALL := askChangeDefaultInstall("Continue with install? Select")
@@ -43,8 +46,6 @@ func main() {
 				os.Exit(1)
 			}
 		}
-	} else if OK_DEFAULT_INSTALL {
-		InstallOracleInstantClient(USER_DOWNLOADS, ORAIC_DST_PATH)
 	} else {
 		os.Exit(1)
 	}
@@ -120,13 +121,13 @@ func downloadOracleInstantClient(url, dest string) (err error) {
 	// Create file
 	out, err := os.Create(dest)
 	if err != nil {
-		return err
+		panic(err)
 	}
 	defer out.Close()
 	// Get the data
 	resp, err := http.Get(url)
 	if err != nil {
-		return err
+		panic(err)
 	}
 	if resp.StatusCode != http.StatusOK {
 		log.Fatalf("Error while downloading: %s", resp.Status)
@@ -135,46 +136,65 @@ func downloadOracleInstantClient(url, dest string) (err error) {
 	// Write response body to file
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		return err
+		panic(err)
 	}
 	return nil
 }
 
 func unzipOracleInstantClient(zipPath, destPath string) string {
+	// Create base folder
+	err := os.MkdirAll(destPath, 0777)
+	if err != nil {
+		log.Fatalf("Impossible to make base dir: %s", err)
+	}
+
 	// Open a zip archive for reading.
 	r, err := zip.OpenReader(zipPath)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer r.Close()
+
 	// Iterate through the files in the archive, printing some of their contents.
 	var DEST_PATH string
 	for k, f := range r.File {
-		if k == 0 {
+		re := regexp.MustCompilePOSIX(`^(instantclient_){1}([0-9]{1,2})_([0-9]{1,2})\/$`)
+		matched := re.Match([]byte(f.Name))
+		if matched {
 			DEST_PATH = f.Name
 		}
-		fmt.Printf(" - Unzipping: %s\n", f.Name)
-		rc, err := f.Open()
-		if err != nil {
-			log.Fatalf("cannot open file n%d in zip: %s", k, err)
-		}
-		outPath := filepath.Join(destPath, f.Name)
-		// Case 1: directory
-		if f.FileInfo().IsDir() {
-			err = os.MkdirAll(outPath, 0777)
+
+		// If current 'f' ends in '/', then it's a dir, and that dir needs created.
+		var outPath string
+		if f.Name[len(f.Name)-1:] == "/" {
+			outPath = filepath.Join(destPath, f.Name)
+			fmt.Println("DIR OUTPATH: " + outPath)
+			err := os.MkdirAll(outPath, 0777)
 			if err != nil {
-				log.Fatalf("impossible to MkdirAll: %s", err)
+				log.Fatalf("Impossible to MkdirAll: %s", err)
 			}
 			continue
-		}
-		// Case 2: file
-		unzippedFile, err := os.Create(outPath)
-		if err != nil {
-			log.Fatalf("impossible to unzip: %s", err)
-		}
-		_, err = io.Copy(unzippedFile, rc)
-		if err != nil {
-			log.Fatalf("cannot copy file n%d: %s", k, err)
+		} else {
+			re := regexp.MustCompile(`^(.*[\\\/])[^\\\/]*$`)
+			outPath = filepath.Join(destPath, re.ReplaceAllString(f.Name, "$1"))
+			err := os.MkdirAll(outPath, 0777)
+			if err != nil {
+				log.Fatalf("Impossible to MkdirAll: %s", err)
+			}
+
+			fmt.Printf(" - Unzipping: %s\n", f.Name)
+			rc, err := f.Open()
+			if err != nil {
+				log.Fatalf("Cannot open file n%d in zip: %s", k, err)
+			}
+			unzippedFile, err := os.Create(filepath.Join(destPath, f.Name))
+			if err != nil {
+				log.Fatalf("Impossible to unzip: %s", err)
+			}
+			_, err = io.Copy(unzippedFile, rc)
+			if err != nil {
+				log.Fatalf("Cannot copy file n%d: %s", k, err)
+			}
 		}
 	}
 	return DEST_PATH
@@ -185,7 +205,7 @@ func setEnvironmentVariable(usrEnvVar, envVarPath string) {
 	psGetEnvCmd := "[System.Environment]::GetEnvironmentVariable('" + usrEnvVar + "', 'User')"
 	currUsrVar, err := exec.Command("powershell", psGetEnvCmd).Output()
 	if err != nil {
-		fmt.Println(err.Error())
+		panic(err)
 	}
 	currUsrVarStr := strings.TrimSuffix(string(currUsrVar), "\r\n")
 
@@ -224,7 +244,7 @@ func setEnvironmentVariable(usrEnvVar, envVarPath string) {
 			psGetEnvCmd := "[System.Environment]::GetEnvironmentVariable('" + usrEnvVar + "', 'User')"
 			usrPath, err := exec.Command("powershell", psGetEnvCmd).Output()
 			if err != nil {
-				fmt.Println(err.Error())
+				panic(err)
 			}
 			envVarDir = strings.TrimSuffix(string(usrPath), "\r\n") + envVarPath + ";"
 			fmt.Println("\t" + usrEnvVar + "=" + envVarDir)
@@ -236,7 +256,7 @@ func setEnvironmentVariable(usrEnvVar, envVarPath string) {
 		psSetEnvCmd := "[Environment]::SetEnvironmentVariable('" + usrEnvVar + "', '" + envVarDir + "' , 'User')"
 		_, err := exec.Command("powershell", psSetEnvCmd).Output()
 		if err != nil {
-			fmt.Println(err.Error())
+			panic(err)
 		}
 		_, exists := os.LookupEnv(usrEnvVar)
 		if exists {
@@ -246,19 +266,20 @@ func setEnvironmentVariable(usrEnvVar, envVarPath string) {
 }
 
 func InstallOracleInstantClient(downloadPath, installPath string) {
+	// Set paths for filename download locations
 	ORAIC_PKG_ZIP_LOCATION := filepath.Join(downloadPath, ORAIC_PKG_NAME)
-	fmt.Println("Downloaded PKG ZIP: " + ORAIC_PKG_ZIP_LOCATION)
 	ORAIC_SDK_ZIP_LOCATION := filepath.Join(downloadPath, ORAIC_SDK_NAME)
-	fmt.Println("Downloaded SDK ZIP: " + ORAIC_SDK_ZIP_LOCATION)
-	ORAIC_PKG_URL := filepath.Join(ORAIC_BASE_URL, ORAIC_PKG_NAME)
-	ORAIC_SDK_URL := filepath.Join(ORAIC_BASE_URL, ORAIC_SDK_NAME)
+	ORAIC_PKG_URL := ORAIC_BASE_URL + ORAIC_PKG_NAME
+	ORAIC_SDK_URL := ORAIC_BASE_URL + ORAIC_SDK_NAME
 
 	// Download LATEST Oracle Instant Client PKG & SDK
+	fmt.Println("Downloading PKG ZIP: " + ORAIC_PKG_ZIP_LOCATION + "...")
 	downloadOracleInstantClient(ORAIC_PKG_URL, ORAIC_PKG_ZIP_LOCATION)
+	fmt.Println("Downloaded SDK ZIP: " + ORAIC_SDK_ZIP_LOCATION + "...")
 	downloadOracleInstantClient(ORAIC_SDK_URL, ORAIC_SDK_ZIP_LOCATION)
 
 	// Unzip Oracle Instant Client PKG & SDK (NOTE: '*_TLD' short for 'Top Level Directory')
-	fmt.Println("Unzipping:", ORAIC_PKG_ZIP_LOCATION)
+	fmt.Println("Unzipping: " + ORAIC_PKG_ZIP_LOCATION + " to " + installPath)
 	ORAIC_PKG_TLD := unzipOracleInstantClient(ORAIC_PKG_ZIP_LOCATION, installPath)
 	fmt.Println("Unzipping:", ORAIC_SDK_ZIP_LOCATION)
 	ORAIC_SDK_TLD := unzipOracleInstantClient(ORAIC_SDK_ZIP_LOCATION, installPath)
@@ -268,12 +289,15 @@ func InstallOracleInstantClient(downloadPath, installPath string) {
 		fmt.Println("Oracle instant client pkg and sdk versions match. Continuing...")
 	} else {
 		fmt.Println("Oracle instant client pkg and sdk versions DO NOT match. Exiting...")
+		fmt.Println("    ORAIC_PKG_TLD: " + ORAIC_PKG_TLD)
+		fmt.Println("    ORAIC_SDK_TLD: " + ORAIC_SDK_TLD)
 		os.Exit(1)
 	}
 
 	OCI_LIB64_ENVAR := filepath.Join(installPath, ORAIC_PKG_TLD)
+	fmt.Println("OCI_LIB64_ENVAR: " + OCI_LIB64_ENVAR)
 	TNS_ADMIN_ENVAR := filepath.Join(OCI_LIB64_ENVAR, "network", "admin")
-	fmt.Println(TNS_ADMIN_ENVAR)
+	fmt.Println("TNS_ADMIN_ENVAR: " + TNS_ADMIN_ENVAR)
 	setEnvironmentVariable("OCI_LIB64", OCI_LIB64_ENVAR)
 	setEnvironmentVariable("TNS_ADMIN", TNS_ADMIN_ENVAR)
 	setEnvironmentVariable("PATH", OCI_LIB64_ENVAR)
