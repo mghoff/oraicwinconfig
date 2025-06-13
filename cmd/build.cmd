179d4c638fdb182bba94fc6d9a37601eeaab87f1
mghoff <mhoff@burnsmcd.com>
2025-06-13 14:43:46 -0400

    rename output folder name of ./cmd/build.cmd to ./bin - clarifying that it houses the compiled binary

--- cmd/build.cmd
+++ cmd/build.cmd
@@ -3,9 +3,11 @@ setlocal enabledelayedexpansion
 
 :: Set version number
 set VERSION=0.1.0
+set EXECUTABLE=bin\oraicwinconfig.exe
+set CHECKSUM_FILE=bin\SHA256SUMS
 
-:: Create dist directory if it doesn't exist
-if not exist "dist" mkdir dist
+:: Create bin directory if it doesn't exist
+if not exist "bin" mkdir bin
 
 :: Get build timestamp
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
@@ -19,16 +21,16 @@ for /f "tokens=3" %%I in ('go version') do set GOVERSION=%%I
 
 :: Build the executable with version information
 echo Building version %VERSION%...
-go build -o dist\oraicwinconfig.exe -ldflags ^
+go build -o %EXECUTABLE% -ldflags ^
 "-X github.com/mghoff/oraicwinconfig/internal.Version=%VERSION% ^
  -X github.com/mghoff/oraicwinconfig/internal.BuildTime=%BUILDTIME% ^
  -X github.com/mghoff/oraicwinconfig/internal.GitCommit=%COMMIT% ^
  -X github.com/mghoff/oraicwinconfig/internal.GoVersion=%GOVERSION%"
 
 :: Generate checksums using certutil
-certutil -hashfile dist\oraicwinconfig.exe SHA256 | findstr /v "hash" | findstr /v "CertUtil" > dist\SHA256SUMS
-for /f "tokens=1" %%a in (dist\SHA256SUMS) do (
-    echo %%a  oraicwinconfig.exe> dist\SHA256SUMS
+certutil -hashfile %EXECUTABLE% SHA256 | findstr /v "hash" | findstr /v "CertUtil" > %CHECKSUM_FILE%
+for /f "tokens=1" %%a in (%CHECKSUM_FILE%) do (
+  echo %%a oraicwinconfig.exe > %CHECKSUM_FILE%
 )
 
 echo Build v%VERSION% complete!
--- cmd/generate-checksums.cmd
+++ cmd/generate-checksums.cmd
@@ -1,18 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set EXECUTABLE=dist\oraicwinconfig.exe
-set CHECKSUM_FILE=dist\SHA256SUMS
+set EXECUTABLE=bin\oraicwinconfig.exe
+set CHECKSUM_FILE=bin\SHA256SUMS
 
-:: Create dist directory if it doesn't exist
-if not exist "dist" mkdir dist
+:: Create bin directory if it doesn't exist
+if not exist "bin" mkdir bin
 
 :: Generate SHA256 checksum using certutil
 certutil -hashfile %EXECUTABLE% SHA256 | findstr /v "hash" | findstr /v "CertUtil" > %CHECKSUM_FILE%
 
 :: Add filename to checksum file
 for /f "tokens=1" %%a in (%CHECKSUM_FILE%) do (
-    echo %%a  %EXECUTABLE%> %CHECKSUM_FILE%
+  echo %%a %EXECUTABLE% > %CHECKSUM_FILE%
 )
 
 echo Generated checksums for %EXECUTABLE%
